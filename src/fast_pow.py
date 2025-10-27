2e21101218212f912b31ff11b3a5f55b9bc41dc4
MariaPentegova <m.pentegova@yandex.ru>
2025-10-27 16:02:04 +0300

    Update fast_pow.py

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,16 @@
 def fastPow(number, power):
-    result = number
-    while power != 1:
-        result *= result
-        power = power // 2
-    return result
+    if power == 0: #если степень равна 0
+        return 1
+    else:
+        result = number
+        k = abs(power) #дополнительная переменная на случай, если степень отрицательная
+        while k != 1:
+            result = result**2
+            k //= 2
+    if power>0: #вывод зависит от того, положительна или отрицательна степень
+        return result
+    else:
+        return 1/result
+print(fastPow(2, -2))
+print(fastPow(-3, 2))
+print(fastPow(-3, -2))
